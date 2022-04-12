47d06e9d9644944b90b52d046da189c3709bd716
donovanodom <dodomdev@gmail.com>
2022-04-12 18:39:44 +0000

    Sync LeetCode submission - Roman to Integer (javascript)

--- problems/roman_to_integer/solution.js
+++ problems/roman_to_integer/solution.js
@@ -1,26 +1,15 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
+const objRtoI = {"I":1,"V":5, "X":10, "L":50,"C":100, "D":500, "M":1000 }
+const romanToInt = (s) => {
+    let arr = s.split('');
+    let answer = 0;
     
-    let sum = 0
-    if(s.indexOf('IV') != -1){  sum -= 2 }
-    if(s.indexOf('IX') != -1){  sum -= 2 }
-    if(s.indexOf('XL') != -1){  sum -= 20 }
-    if(s.indexOf('XC') != -1){  sum -= 20 }
-    if(s.indexOf('CD') != -1){  sum -= 200 }
-    if(s.indexOf('CM') != -1){  sum -= 200 }
-    
-    for(let i = 0; i < s.length; i++){
-        if(s.charAt(i) == 'I'){ sum += 1 }
-        if(s.charAt(i) == 'V'){ sum += 5 }
-        if(s.charAt(i) == 'X'){ sum += 10 }
-        if(s.charAt(i) == 'L'){ sum += 50 }
-        if(s.charAt(i) == 'C'){ sum += 100 }
-        if(s.charAt(i) == 'D'){ sum += 500 }
-        if(s.charAt(i) == 'M'){ sum += 1000 }
+    for (i=0; i<s.length; i++)
+    {
+        if (s[i+1] && objRtoI[s[i]] < objRtoI[s[i+1]]) {
+            answer = answer-objRtoI[s[i]];
+        }else{
+            answer = answer+objRtoI[s[i]];
+        }
     }
-   
-    return sum
+    return answer;
 };

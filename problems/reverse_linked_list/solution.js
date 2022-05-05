3344ff8f47d3f28ea4cf9047278c94ea312bee30
donovanodom <dodomdev@gmail.com>
2022-05-05 19:03:18 +0000

    Sync LeetCode submission - Reverse Linked List (javascript)

--- problems/reverse_linked_list/solution.js
+++ problems/reverse_linked_list/solution.js
@@ -1,13 +1,10 @@
 const reverseList = (head) => {
-    // p, c, & f will represent previous, current & following nodes respectively 
-    let p = null
-    let c = head
-    let f = head
-    while(c){
-        f = f.next // Set following equal to following.next
-        c.next = p // Set current.next equal to previous
-        p = c // Set previous equal to current
-        c = f // Set current equal to following
+    let previous = null, current = head, following = head
+    while(following){
+        following = following.next
+        current.next = previous
+        previous = current
+        current = following
     }
-    return p
-};
+    return previous
+}

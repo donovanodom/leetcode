0246af6a1bd7ba2a0c60f94ca103384ff4860e28
donovanodom <dodomdev@gmail.com>
2022-05-07 19:15:03 +0000

    Sync LeetCode submission - Reverse Integer (javascript)

--- problems/reverse_integer/solution.js
+++ problems/reverse_integer/solution.js
@@ -1,13 +1,11 @@
 const reverse = (x) => {
-    let result = []
-    const num =  String(x)
-    for(i = num.length -1; i >= 0; i--){
-        if(num[i] != '-'){
-            result.push(num[i])
-        }else{
-            result.unshift(num[i])
-        }
+    const isEven = x < 0 ? false : true
+    x = Math.abs(x)
+    x = Array.from(String(x)).reverse()
+    x = x.map((x) => parseInt(x)).join('')
+    if(-2147483648 <= x && x <= 2147483647){
+        return isEven ? x : 0 - x
+    }else{
+        return 0
     }
-    result = result.join('')
-    return Math.abs((result)) < 2147483647 ? result : 0
-};  
+}

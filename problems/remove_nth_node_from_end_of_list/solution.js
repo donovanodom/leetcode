b653a222fab4afa9706e75d572388515dcb70f6b
donovanodom <dodomdev@gmail.com>
2022-05-10 18:54:41 +0000

    Sync LeetCode submission - Remove Nth Node From End of List (javascript)

--- problems/remove_nth_node_from_end_of_list/solution.js
+++ problems/remove_nth_node_from_end_of_list/solution.js
@@ -3,21 +3,18 @@ const removeNthFromEnd = (head, n) => {
         head = head.next
         return head
     }
-    let arr = []
-    let last = head
+    let array = []
     while(head){
-       arr.push(head.val)
-       head = head.next
+        array.push(head.val)
+        head = head.next
     }
-    arr.splice(arr.length - n, 1)
-    head = last
-    for(i = 0; i < arr.length; i++){
-        last.val = arr[i]
-        if(i == arr.length - 1){
-            last.next = null
-        }else{
-            last = last.next
-        }
+    array.splice(array.length - n, 1)
+    let answer = new ListNode
+    let dummy = answer
+    console.log(array)
+    for(i = 0; i < array.length; i++){
+        dummy.next = new ListNode(array[i])
+        dummy = dummy.next
     }
-    return head
+    return answer.next
 }

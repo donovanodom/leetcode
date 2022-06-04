0c463350e997f57230467456190dae5117604fc4
donovanodom <dodomdev@gmail.com>
2022-06-04 20:43:06 +0000

    Sync LeetCode submission - Single Number (javascript)

--- problems/single_number/solution.js
+++ problems/single_number/solution.js
@@ -1,9 +1,8 @@
 const singleNumber = (nums) => {
-    if(nums.length == 1) return nums[0]
-    nums = nums.sort(function(a,b){return a - b})
-    let pair = 1
-    for(i = 0; i < nums.length; i += 2){
-        if(nums[i] != nums[pair]) return nums[i]
-        pair += 2
+    let i = 0, answer = 0
+    while(i < nums.length){
+        answer = answer ^ nums[i]
+        i++
     }
+    return answer
 }

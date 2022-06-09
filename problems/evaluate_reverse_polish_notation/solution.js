d459c3f8210276a94465686bc8d8deaf3e71320f
donovanodom <dodomdev@gmail.com>
2022-06-09 19:23:27 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (javascript)

---
+++ problems/evaluate_reverse_polish_notation/solution.js
@@ -0,0 +1,28 @@
+const evalRPN = (tokens) => {
+    let i = 0, stack = []
+    while(i < tokens.length){
+        if(!isNaN(tokens[i])){
+            tokens[i] = Number(tokens[i])
+            stack.push(tokens[i])
+        }else{
+            let b = stack.pop(), a = stack.pop()
+             switch(tokens[i]){
+                case '+':
+                    stack.push(a + b)
+                break
+                case '-':
+                    stack.push(a - b)
+                break
+                case '/':
+                    stack.push(~~(a / b))
+                break
+                case '*':
+                    stack.push(a * b)
+                break        
+            }    
+        }
+        i++
+    }
+    return stack[0]
+}
+

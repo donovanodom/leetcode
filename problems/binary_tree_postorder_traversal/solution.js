894ea696b9afbb8b36c5ab2fdf79411bd46ced14
donovanodom <dodomdev@gmail.com>
2022-06-11 17:45:05 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (javascript)

--- problems/binary_tree_postorder_traversal/solution.js
+++ problems/binary_tree_postorder_traversal/solution.js
@@ -1,7 +1,7 @@
-const postorderTraversal = (root, list = []) => {
-    if(!root) return list
-    postorderTraversal(root.left, list)
-    postorderTraversal(root.right, list)
-    list.push(root.val)
-    return list
+const postorderTraversal = (root, answer = []) => {
+    if(!root) return []
+    postorderTraversal(root.left, answer)
+    postorderTraversal(root.right, answer)
+    answer.push(root.val)
+    return answer
 }

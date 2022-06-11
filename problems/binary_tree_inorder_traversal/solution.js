ed7b369b9b5e95466f08ccf2c65a8fb3f67d8d8b
donovanodom <dodomdev@gmail.com>
2022-06-11 17:40:03 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (javascript)

--- problems/binary_tree_inorder_traversal/solution.js
+++ problems/binary_tree_inorder_traversal/solution.js
@@ -1,7 +1,7 @@
-const inorderTraversal = (root, list = []) => {
-    if(!root) return list
-    inorderTraversal(root.left, list)
-    list.push(root.val)
-    inorderTraversal(root.right, list)
-    return list
+const inorderTraversal = (root, answer = []) => {
+    if(!root) return []
+    inorderTraversal(root.left, answer)
+    answer.push(root.val)
+    inorderTraversal(root.right, answer)
+    return answer
 }

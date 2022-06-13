87b5613e0720c454608d590fafcbc93303651112
donovanodom <dodomdev@gmail.com>
2022-06-13 16:30:00 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (javascript)

--- problems/binary_tree_level_order_traversal/solution.js
+++ problems/binary_tree_level_order_traversal/solution.js
@@ -1,8 +1,19 @@
-const levelOrder = (root, answer = [], depth = 0) => {
-    if(!root) return []
-    if(!answer[depth]) answer[depth] = []
-    answer[depth].push(root.val)
-    levelOrder(root.left, answer, depth + 1)
-    levelOrder(root.right, answer, depth + 1)
-    return answer
+const levelOrder = (root) => {
+    if (!root) return []
+    let queue = [[root, 0]], map = {}
+    while(queue.length > 0){
+        for(const [node, x] of queue){
+            if(!map[x]) map[x] = []
+            map[x].push([node.val])
+        }
+        const len = queue.length
+        let i = 0
+        while(i < len){
+            let [node, x] = queue.shift()
+            if(node.left) queue.push([node.left, x + 1])
+            if(node.right) queue.push([node.right, x + 1])
+            i++
+        }
+    }
+    return Object.keys(map).map((key) => map[key])
 }

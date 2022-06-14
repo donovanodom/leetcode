c5420eeb953e8df405cac2b42b6659ba88c5e214
donovanodom <dodomdev@gmail.com>
2022-06-14 00:47:08 +0000

    Sync LeetCode submission - N-ary Tree Level Order Traversal (javascript)

---
+++ problems/n-ary_tree_level_order_traversal/solution.js
@@ -0,0 +1,18 @@
+const levelOrder = (root) => {
+    if(!root) return []
+    let q = [[root, 0]], map = {}
+    while(q.length > 0){
+        for(const [node, x] of q){
+            if(!map[x]) map[x] = []
+            map[x].push(node.val)
+        }
+        const len = q.length
+        let i = 0
+        while(i < len){
+            let [node, x] = q.shift()
+            node.children.forEach((child) => q.push([child, x + 1]))
+            i++
+        }
+    }
+    return Object.keys(map).map((key) => map[key])
+}

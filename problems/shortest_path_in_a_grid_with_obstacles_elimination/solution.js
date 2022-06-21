2d8692e3fa1ac7679a7db8ca0facb910438ed6ad
donovanodom <dodomdev@gmail.com>
2022-06-21 15:48:04 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (javascript)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.js
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.js
@@ -1,15 +1,14 @@
 const shortestPath = (grid, k) => {
     let row = grid[0].length, col = grid.length
-    if(row * col == 1) return 0
-    const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+    if(row * grid == 1) return 0
+    let directions = [[1,0],[-1,0],[0,1],[0,-1]]
     let q = [[0,0,k]], nextQ = [], moves = 0
-    let visited = new Set()
-    visited.add(`0:0:${k}`)
+    let visited = new Set([`0:0:${k}`])
     while(q.length){
         let [x,y,k] = q.shift()
         if(x == row - 1 && y == col - 1) return moves
-        for(const [directionX,directionY] of directions){
-            let nextX = x + directionX, nextY = y + directionY
+        for(const [directionX, directionY] of directions){
+            let nextX = directionX + x, nextY = directionY + y
             if(nextX < 0 || nextY < 0) continue
             if(nextX >= row || nextY >= col) continue
             let obstacle = grid[nextY][nextX]

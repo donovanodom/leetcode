60d8d8840aa633422868df7afa7a63ea82f02306
donovanodom <dodomdev@gmail.com>
2022-06-23 16:50:36 +0000

    Sync LeetCode submission - Roman to Integer (javascript)

--- problems/roman_to_integer/solution.js
+++ problems/roman_to_integer/solution.js
@@ -1,15 +1,13 @@
-const objRtoI = {"I":1,"V":5, "X":10, "L":50,"C":100, "D":500, "M":1000 }
 const romanToInt = (s) => {
-    let arr = s.split('');
-    let answer = 0;
-    
-    for (i=0; i<s.length; i++)
-    {
-        if (s[i+1] && objRtoI[s[i]] < objRtoI[s[i+1]]) {
-            answer = answer-objRtoI[s[i]];
+    const romans = {"I":1,"V":5, "X":10, "L":50,"C":100, "D":500, "M":1000 }
+    let answer = 0, i = 0
+    while(i < s.length){
+        if(s[i+1] && romans[s[i]] < romans[s[i+1]]){
+            answer = answer - romans[s[i]]
         }else{
-            answer = answer+objRtoI[s[i]];
+            answer = answer + romans[s[i]]
         }
+        i++
     }
-    return answer;
-};
+    return answer
+}

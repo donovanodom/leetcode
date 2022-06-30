062c0b696070e13e9e14acad67d245b7999b86fd
donovanodom <dodomdev@gmail.com>
2022-06-30 20:50:30 +0000

    Sync LeetCode submission - Valid Parentheses (javascript)

--- problems/valid_parentheses/solution.js
+++ problems/valid_parentheses/solution.js
@@ -1,10 +1,10 @@
 const isValid = (s) => {
     let stack = [], i = 0
-    while(i < s.length){
-        let char = stack[stack.length - 1]
+    while(i<s.length){
+        let char = stack[stack.length-1]
         if(s[i] == '(' || s[i] == '{' || s[i] == '['){
             stack.push(s[i])
-        }else if(s[i] == ')' && char == '(' || s[i] == '}' && char == '{' || s[i] == ']' && char == '['){
+        }else if(char == '(' && s[i] ==')' || char == '{' && s[i] =='}' || char == '[' && s[i] ==']'){
             stack.pop()
         }else{
             return false

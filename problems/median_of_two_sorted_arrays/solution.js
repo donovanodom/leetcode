1ccef3cecb516b84578d814f60f148761cc4a58d
donovanodom <dodomdev@gmail.com>
2022-06-30 16:31:14 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (javascript)

--- problems/median_of_two_sorted_arrays/solution.js
+++ problems/median_of_two_sorted_arrays/solution.js
@@ -1,15 +1,15 @@
 const findMedianSortedArrays = (nums1, nums2) => {
     let merged = []
-    if (!nums1) merged = nums2
-    if (!nums2) merged = nums1
-    if (nums1 && nums2) merged = nums1.concat(nums2)
-    merged.sort(function(a, b){return a-b})
-    const length = merged.length
-    if(length % 2 != 0) {
-        let i = length / 2
+    if(!nums1) merged = nums2
+    if(!nums2) merged = nums1
+    if(nums1 && nums2) merged = [...nums1, ...nums2]
+    merged.sort((a,b)=>{return a-b})
+    const len = merged.length
+    if(len % 2 != 0){
+        let i = len / 2
         return merged[Math.floor(i)]
     }else{
-        let i = Math.floor(length / 2)
+        let i = Math.floor(len / 2)
         let j = i - 1
         return (merged[i] + merged[j]) / 2
     }

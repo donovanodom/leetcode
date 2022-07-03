419da5cb1b48d820b081cf35838d1174284c8509
donovanodom <dodomdev@gmail.com>
2022-07-03 22:30:50 +0000

    Sync LeetCode submission - N-ary Tree Preorder Traversal (javascript)

--- problems/n-ary_tree_preorder_traversal/solution.js
+++ problems/n-ary_tree_preorder_traversal/solution.js
@@ -1,6 +1,6 @@
 const preorder = (root, list = []) => {
-    if(!root) return []
+   if(!root) return []
     list.push(root.val)
-    root.children.forEach((child) => preorder(child, list))
+    root.children.map((child) => preorder(child, list))
     return list
 }

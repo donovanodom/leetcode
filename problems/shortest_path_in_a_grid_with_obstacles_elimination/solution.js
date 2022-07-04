d70dbb181dfd2856c4e0229b6bc4ea2dc95fb78b
donovanodom <dodomdev@gmail.com>
2022-07-04 15:41:48 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (javascript)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.js
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.js
@@ -1,23 +1,22 @@
-const shortestPath = (grid, k) => {
+const shortestPath = function(grid, k) {
     let row = grid[0].length, col = grid.length
-    if(row * grid == 1) return 0
-    let directions = [[1,0],[-1,0],[0,1],[0,-1]]
-    let q = [[0,0,k]], nextQ = [], moves = 0
-    let visited = new Set([`0:0:${k}`])
+    if(row * col == 1) return 0
+    let q = [[0,0,k]], nextQ = [], visited = new Set(`0:0:${k}`), moves = 0
+    const directions = [[1,0],[-1,0],[0,1],[0,-1]]
     while(q.length){
         let [x,y,k] = q.shift()
         if(x == row - 1 && y == col - 1) return moves
-        for(const [directionX, directionY] of directions){
+        for(const [directionX,directionY] of directions){
             let nextX = directionX + x, nextY = directionY + y
             if(nextX < 0 || nextY < 0) continue
-            if(nextX >= row || nextY >= col) continue
+            if(nextX > row - 1 || nextY > col - 1) continue
             let obstacle = grid[nextY][nextX]
             if(obstacle && k < 1) continue
             let nextK = obstacle ? k - 1 : k
             let nextP = [nextX,nextY,nextK], nextV = `${nextX}:${nextY}:${nextK}`
             if(!visited.has(nextV)){
-                nextQ.push(nextP)
                 visited.add(nextV)
+                nextQ.push(nextP)
             }
         }
         if(!q.length){
@@ -26,5 +25,5 @@ const shortestPath = (grid, k) => {
             moves++
         }
     }
-    return -1
+    return - 1
 }

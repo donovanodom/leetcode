33553bbfa8adc52720ab95eaa6be460a8fd12bd9
donovanodom <dodomdev@gmail.com>
2022-07-15 14:56:13 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (javascript)

---
+++ problems/find_if_path_exists_in_graph/solution.js
@@ -0,0 +1,19 @@
+const validPath = function(n, edges, source, destination) {
+    const adjList = {}, visited = new Set()
+    edges.forEach((edge) => {
+        let x = edge[0], y = edge[1]
+        if(!adjList[x]) adjList[x] = []
+        adjList[x].push(y)
+        if(!adjList[y]) adjList[y] = []
+        adjList[y].push(x)        
+    })
+    const dfs = (node) => {
+        visited.add(node)
+        adjList[node] && adjList[node].forEach((next) => {
+            if(!visited.has(next)) dfs(next)
+        })      
+    }
+    dfs(source)
+    return visited.has(destination)
+}
+

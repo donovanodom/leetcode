a71d4d12427ddbc3b9c1765f68d00046c3a7123e
donovanodom <dodomdev@gmail.com>
2022-07-19 19:46:16 +0000

    Sync LeetCode submission - Detonate the Maximum Bombs (javascript)

--- problems/detonate_the_maximum_bombs/solution.js
+++ problems/detonate_the_maximum_bombs/solution.js
@@ -18,7 +18,6 @@ const maximumDetonation = (bombs) => {
                     visited.add(adjList[current][j])
                     subMax++
                     q.push(adjList[current][j])
-                   
                 }
             }
             if(!q.length) max = Math.max(subMax, max)    
@@ -26,3 +25,40 @@ const maximumDetonation = (bombs) => {
     }
     return max
 }
+
+
+
+    // const dfs = (point, visited = new Set()) => {
+    //     let subMax = 0
+    //     adjList[point].forEach((neighbor) => {
+    //         if(!visited.has(neighbor)){
+    //             visited.add(neighbor)
+    //             subMax++
+    //             subMax += dfs(neighbor, visited)
+    //         }   
+    //     }) 
+    //     return subMax      
+    // }
+    // for(i = 0; i < bombs.length; i++){
+    //     max = Math.max(dfs(i), max)
+    //     if(max == bombs.length) return max
+    // }
+    // return max
+
+    // for(i = 0; i < bombs.length; i++){
+    //     if(max == bombs.length) return max
+    //     let q = [i], subMax = 0, visited = new Set()
+    //     const len = q.length
+    //     while(q.length){
+    //         let current = q.shift()
+    //         for(j = 0; j < adjList[current].length; j++){
+    //             if(!visited.has(adjList[current][j])){
+    //                 visited.add(adjList[current][j])
+    //                 subMax++
+    //                 q.push(adjList[current][j])
+    //             }
+    //         }
+    //         if(!q.length) max = Math.max(subMax, max)    
+    //     }    
+    // }
+    // return max

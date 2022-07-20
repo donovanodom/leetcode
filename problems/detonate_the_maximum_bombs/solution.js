09762373e3c4dff7f41ea2603fdc651b77082f78
donovanodom <dodomdev@gmail.com>
2022-07-20 14:21:10 +0000

    Sync LeetCode submission - Detonate the Maximum Bombs (javascript)

--- problems/detonate_the_maximum_bombs/solution.js
+++ problems/detonate_the_maximum_bombs/solution.js
@@ -26,39 +26,31 @@ const maximumDetonation = (bombs) => {
     return max
 }
 
+// --Recursive Solution--
 
-
-    // const dfs = (point, visited = new Set()) => {
-    //     let subMax = 0
-    //     adjList[point].forEach((neighbor) => {
-    //         if(!visited.has(neighbor)){
-    //             visited.add(neighbor)
-    //             subMax++
-    //             subMax += dfs(neighbor, visited)
-    //         }   
-    //     }) 
-    //     return subMax      
-    // }
-    // for(i = 0; i < bombs.length; i++){
-    //     max = Math.max(dfs(i), max)
-    //     if(max == bombs.length) return max
-    // }
-    // return max
-
-    // for(i = 0; i < bombs.length; i++){
-    //     if(max == bombs.length) return max
-    //     let q = [i], subMax = 0, visited = new Set()
-    //     const len = q.length
-    //     while(q.length){
-    //         let current = q.shift()
-    //         for(j = 0; j < adjList[current].length; j++){
-    //             if(!visited.has(adjList[current][j])){
-    //                 visited.add(adjList[current][j])
-    //                 subMax++
-    //                 q.push(adjList[current][j])
-    //             }
-    //         }
-    //         if(!q.length) max = Math.max(subMax, max)    
-    //     }    
-    // }
-    // return max
+// const maximumDetonation = (bombs) => {
+//     let adjList = {}, max = 0
+//     for(i = 0; i < bombs.length; i++){
+//         if(!adjList[i]) adjList[i] = []
+//         for(j = 0; j < bombs.length; j++){
+//             let [x,y,r] = bombs[i], h = bombs[j][0], k = bombs[j][1]
+//             if (Math.sqrt(Math.pow(x-h, 2) + Math.pow(y-k, 2)) <= r) adjList[i].push(j)
+//         }
+//     }
+//     const dfs = (point, visited = new Set()) => {
+//         let subMax = 0
+//         adjList[point].forEach((neighbor) => {
+//             if(!visited.has(neighbor)){
+//                 visited.add(neighbor)
+//                 subMax++
+//                 subMax += dfs(neighbor, visited)
+//             }   
+//         }) 
+//         return subMax      
+//     }
+//     for(i = 0; i < bombs.length; i++){
+//         max = Math.max(dfs(i), max)
+//         if(max == bombs.length) return max
+//     }
+//     return max
+// }

3783da4f594466de3f4939590105c5fc1b76ba71
donovanodom <dodomdev@gmail.com>
2022-07-21 21:17:53 +0000

    Sync LeetCode submission - Decompress Run-Length Encoded List (javascript)

--- problems/decompress_run-length_encoded_list/solution.js
+++ problems/decompress_run-length_encoded_list/solution.js
@@ -1,7 +1,16 @@
 const decompressRLElist = function(nums) {
-    let answer = []
-    for(i = 0; i < nums.length; i += 2){       
-        answer = [...answer,...Array(nums[i]).fill(nums[i+1])]
+    if(nums.length == 2) return Array(nums[0]).fill(nums[1])
+    let answer = [], l = 0, r = nums.length - 2
+    while(l < r){          
+        if(l < r && r - 4 >= l){
+            answer = [...answer,...Array(nums[l]).fill(nums[l+1])]
+            l += 2
+        }else{
+            answer = [...answer,...Array(nums[l]).fill(nums[l+1]),...Array(nums[r]).fill(nums[r+1])]
+            r -= 2
+            l += 2
+        }
+        
     }
     return answer
 };

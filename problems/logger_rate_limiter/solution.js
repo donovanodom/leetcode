80dd98df27add73ff65d1f8d53722d3bfe902dcd
donovanodom <dodomdev@gmail.com>
2022-07-22 16:58:45 +0000

    Sync LeetCode submission - Logger Rate Limiter (javascript)

--- problems/logger_rate_limiter/solution.js
+++ problems/logger_rate_limiter/solution.js
@@ -1,13 +1,14 @@
 
 const Logger = function() {
-    this.messages = {}
+    this.messages = new Map()
 }
 
 
 Logger.prototype.shouldPrintMessage = function(timestamp, message) {
-    if(!this.messages[message]) this.messages[message] = []
-    let current = this.messages[message]
+    if(!this.messages.has(message)) this.messages.set(message, [])
+    let current = this.messages.get(message)
     if(current.length < 1 || current[current.length - 1] + 10 <= timestamp){
+        current.shift()
         current.push(timestamp)
         return true
     } 

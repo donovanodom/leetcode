4d953acb02bfab74e267260eca7f7085a6e8841c
donovanodom <dodomdev@gmail.com>
2022-07-23 19:19:42 +0000

    Sync LeetCode submission - Missing Number (javascript)

--- problems/missing_number/solution.js
+++ problems/missing_number/solution.js
@@ -1,18 +1,8 @@
 const missingNumber = function(nums) {
-    const nums2 = [...Array(nums.length + 1).keys()]
-    nums.sort((a,b) => a-b)
-    if(nums[0] != 0) return 0
-    if(nums[nums.length - 1] != nums.length) return nums.length
-    let l = 0, r = nums.length - 1, m = Math.floor(nums.length/2)
-    while(l <= r){
-        if(nums[l] + 1 != nums[l + 1]) return nums[l] + 1
-        if(nums[r] - 1 != nums[r - 1]) return nums[r] - 1
-        if(nums[m] != nums2[m]){
-            r = m
-            m = Math.floor((r+l)/2)
-        }else if(nums[m] == nums2[m]){
-            l = m
-            m = Math.floor((r+l)/2)
-        } 
+    let sum1 = 0, sum2 = 0
+    for(i = 0; i < nums.length + 1; i++){
+        sum1 += i
+        if(i < nums.length) sum2 += nums[i]
     }
+    return sum1 - sum2
 }

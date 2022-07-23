e475e2f907ae5037e00f475689db98a50d464547
donovanodom <dodomdev@gmail.com>
2022-07-23 18:06:41 +0000

    Sync LeetCode submission - Excel Sheet Column Number (javascript)

---
+++ problems/excel_sheet_column_number/solution.js
@@ -0,0 +1,8 @@
+const titleToNumber = function(columnTitle) {
+    const alpha = Array(26).fill(1).map(( _,i) => String.fromCharCode(65 + i))
+    let col = 0
+    for(i = 0; i < columnTitle.length; i++){
+        col = col * 26 + alpha.indexOf(columnTitle[i]) + 1
+    }
+    return col
+}

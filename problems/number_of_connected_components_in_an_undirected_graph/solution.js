531657ba23a7cfe7bfc915a522363719112a8e66
donovanodom <dodomdev@gmail.com>
2022-08-02 16:11:34 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (javascript)

--- problems/number_of_connected_components_in_an_undirected_graph/solution.js
+++ problems/number_of_connected_components_in_an_undirected_graph/solution.js
@@ -1,26 +1,51 @@
 const countComponents = function(n, edges) {
-    let adjList = {}, visited = new Set(), count = 0
-    for(i = 0; i < n; i++){
-        adjList[i] = []
+    const parents = Array(n).fill().map((_,i) => i)
+    const find = (node) => {
+        if(parents[node] == node){
+            return parents[node]
+        }else{
+            return find(parents[node])
+        }
     }
-    edges.forEach((edge) => {
-        let x = edge[0], y = edge[1]
-        adjList[x].push(y)
-        adjList[y].push(x)        
-    })
-
-    for(let node in adjList){
-        node = Number(node)
-        if(traverse(node,adjList,visited)) count += 1
+    const union = (x,y) => {
+        let repx = find(x), repy = find(y)
+        if(repx != repy){
+            parents[repy] = repx
+        }
     }
-    return count
+    for(const [a,b] of edges){
+        union(a,b)
+    }
+    let answer = 0
+    for(i = 0; i < parents.length; i++){
+        if(parents[i] == i) answer++
+    }
+    return answer
 }
 
-const traverse = (node, adjList, visited) => {
-    if(visited.has(node)) return false
-    visited.add(node)
-    for(const next of adjList[node]){
-        traverse(next, adjList, visited)
-    }
-    return true
-}
+// const countComponents = function(n, edges) {
+//     let adjList = {}, visited = new Set(), count = 0
+//     for(i = 0; i < n; i++){
+//         adjList[i] = []
+//     }
+//     edges.forEach((edge) => {
+//         let x = edge[0], y = edge[1]
+//         adjList[x].push(y)
+//         adjList[y].push(x)        
+//     })
+
+//     for(let node in adjList){
+//         node = Number(node)
+//         if(traverse(node,adjList,visited)) count += 1
+//     }
+//     return count
+// }
+
+// const traverse = (node, adjList, visited) => {
+//     if(visited.has(node)) return false
+//     visited.add(node)
+//     for(const next of adjList[node]){
+//         traverse(next, adjList, visited)
+//     }
+//     return true
+// }

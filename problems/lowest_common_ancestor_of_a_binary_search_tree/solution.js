d8154c98dc95a40bd0d4fb72a34994a66cbbdd0a
donovanodom <dodomdev@gmail.com>
2022-08-12 14:03:33 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree (javascript)

--- problems/lowest_common_ancestor_of_a_binary_search_tree/solution.js
+++ problems/lowest_common_ancestor_of_a_binary_search_tree/solution.js
@@ -1,9 +1,22 @@
-const lowestCommonAncestor = (root, p, q) => {
-    if(p.val < root.val && q.val < root.val){
-        return lowestCommonAncestor(root.left, p, q)
-    }else if(p.val > root.val && q.val > root.val){
-        return lowestCommonAncestor(root.right, p, q)
-    }else{
+const lowestCommonAncestor = function(root, p, q) {
+    if(!root || root == p || root == q) return root
+    let left = lowestCommonAncestor(root.left, p, q) 
+    let right = lowestCommonAncestor(root.right, p, q) 
+    if(left && right){
         return root
+    }else if(left){
+        return left
+    }else{
+        return right
     }
 }
+
+// const lowestCommonAncestor = (root, p, q) => {
+//     if(p.val < root.val && q.val < root.val){
+//         return lowestCommonAncestor(root.left, p, q)
+//     }else if(p.val > root.val && q.val > root.val){
+//         return lowestCommonAncestor(root.right, p, q)
+//     }else{
+//         return root
+//     }
+// }

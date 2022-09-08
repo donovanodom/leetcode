4ed8bcadca8ee3604334a066d4170dc93564860a
donovanodom <dodomdev@gmail.com>
2022-09-08 14:13:35 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (python3)

---
+++ problems/binary_tree_inorder_traversal/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        left = self.inorderTraversal(root.left)
+        right = self.inorderTraversal(root.right)
+        return left + [root.val] + right
+        

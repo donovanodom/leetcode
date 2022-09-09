ac2303d4e0d7dd318bf75928ae9238d7babac1ff
donovanodom <dodomdev@gmail.com>
2022-09-09 21:02:36 +0000

    Sync LeetCode submission - N-Repeated Element in Size 2N Array (javascript)

--- problems/n-repeated_element_in_size_2n_array/solution.js
+++ problems/n-repeated_element_in_size_2n_array/solution.js
@@ -1,9 +1,8 @@
 const repeatedNTimes = function(nums) {
-    let n = nums.length / 2, obj = {}, counts = {}
+    let n = nums.length / 2, obj = {}
     for(const num of nums){
         if(!obj[num]) obj[num] = 0
         obj[num]++
-        counts[obj[num]] = num
+        if(obj[num] == n) return num
     }
-    return counts[n]
 }

5f24811290e2ba8d1724e47c3b284cbd068b8917
donovanodom <dodomdev@gmail.com>
2022-09-09 21:26:34 +0000

    Sync LeetCode submission - Largest Perimeter Triangle (javascript)

---
+++ problems/largest_perimeter_triangle/solution.js
@@ -0,0 +1,7 @@
+const largestPerimeter = function(nums) {
+    nums.sort((a,b) => b - a)
+    for(i = 0; i < nums.length - 2; i++){
+        if(nums[i] < nums[i+1] + nums[i+2]) return nums[i] + nums[i+1] + nums[i+2]
+    }
+    return 0
+}

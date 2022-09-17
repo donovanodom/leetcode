ae22d0d57994df8ff879eac6b25460ad98ee0979
donovanodom <dodomdev@gmail.com>
2022-09-17 18:19:52 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number to Zero (javascript)

---
+++ problems/number_of_steps_to_reduce_a_number_to_zero/solution.js
@@ -0,0 +1,12 @@
+const numberOfSteps = function(num) {
+    let count = 0
+    while(num > 0){
+        if(num % 2 == 0){
+            num /= 2
+        }else{
+            num--
+        }
+        count++
+    }
+    return count
+}

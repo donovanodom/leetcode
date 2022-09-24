9687fe52915b71082febf756a14a37f46cd54675
donovanodom <dodomdev@gmail.com>
2022-09-24 19:20:01 +0000

    Sync LeetCode submission - Slowest Key (javascript)

--- problems/slowest_key/solution.js
+++ problems/slowest_key/solution.js
@@ -1,11 +1,14 @@
 const slowestKey = function(r, k) {
-    let obj = {}, max = r[0]
-    obj[k[0]] = r[0]
+    let obj = {}, max = r[0], ans = k[0]
     for(i = 1; i < r.length; i++){
-        if(!obj[k[i]]) obj[k[i]] = 0
-        obj[k[i]] = Math.max(r[i] - r[i - 1], obj[k[i]])
-        max = Math.max(max, obj[k[i]])
+        let dur = r[i] - r[i - 1]
+        const prev = max
+        max = Math.max(dur, max)
+        if(dur == prev){
+            ans = ans.charCodeAt(0) > k.charCodeAt(i) ? ans : k[i]
+        }else if(max == dur){
+            ans = k[i]
+        }
     }
-    let ans = Object.keys(obj).filter((x) => obj[x] == max).sort()
-    return ans.pop()
+    return ans
 }

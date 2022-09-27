abd034b2c45fb592f16c0771a4ba2be704ae1fc2
donovanodom <dodomdev@gmail.com>
2022-09-27 19:29:55 +0000

    Sync LeetCode submission - Count of Matches in Tournament (javascript)

---
+++ problems/count_of_matches_in_tournament/solution.js
@@ -0,0 +1,13 @@
+const numberOfMatches = function(n) {
+    let matches = 0
+    while(n > 1){
+        if(n % 2 == 0){
+            matches += n / 2
+            n /= 2
+        }else{
+            matches += (n - 1) / 2
+            n = ((n - 1) / 2) + 1
+        }
+    }
+    return matches
+}
